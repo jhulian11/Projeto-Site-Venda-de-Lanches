d83cf2d7f4bdc79435b1ea636210fe3212e52956
jhulian11 <jhuliangamer@gmail.com>
2023-01-24 16:04:52 -0300

    Classes Lanche e Categoria criadas

---
+++ LanchesMac/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿namespace LanchesMac.Models
+{
+    public class Categoria
+    {
+        public int  Id { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+
+        public List<Lanche> Lanches { get; set; }
+
+        public Categoria()
+        {
+            Lanches = new List<Lanche>();
+        }
+    }
+}
