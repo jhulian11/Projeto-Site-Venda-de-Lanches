0e4a30c8c0cb5f5c5ef465c4189e6687c0f3256a
jhulian11 <jhuliangamer@gmail.com>
2023-01-25 15:40:46 -0300

    Creating Lanche Repository

---
+++ LanchesMac/Repositories/LancheRepository.cs
@@ -0,0 +1,23 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Repositories
+{
+    public class LancheRepository: ILancheRepository
+    {
+        private readonly AppDbContext _context;
+
+        public LancheRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Lanche> Lanches => _context.Lanches.Include(l => l.Categoria);
+        public IEnumerable<Lanche> LanchesPreferidos =>  _context.Lanches.Where(l => l.IsLanchePreferido).Include(l => l.Categoria);
+
+        public Lanche GetLancheById(int id) => _context.Lanches.FirstOrDefault(l=> l.Id == id);
+
+    }
+}
