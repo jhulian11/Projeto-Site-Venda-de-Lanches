990e56501c4348409db8c77fc612ccb6ed8e5f17
jhulian11 <jhuliangamer@gmail.com>
2023-05-30 17:27:18 -0300

    Imporing Identity and creating Acc Controller

---
+++ LanchesMac/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace LanchesMac.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = await _userManager.FindByEmailAsync(model.UserName);
+
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, model.Password, false, false);
+                if (result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(model.ReturnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    return Redirect(model.ReturnUrl);
+                }
+            }
+            ModelState.AddModelError("", "Falha ao realizar o login !");
+            return View(model);
+        }
+
+    }
+}
---
+++ LanchesMac/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMac.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o nome")]
+        [Display(Name = "Usuario")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Informe a senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }  
+        public string ReturnUrl { get; set; }  
+    }
+}
