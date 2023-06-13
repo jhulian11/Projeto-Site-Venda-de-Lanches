5aeec2a79287189256e286fd384ee8af15bc1a65
jhulian11 <jhuliangamer@gmail.com>
2023-06-13 16:41:12 -0300

    Creating Login View

--- LanchesMac/Controllers/AccountController.cs
+++ LanchesMac/Controllers/AccountController.cs
@@ -24,6 +24,7 @@ namespace LanchesMac.Controllers
             });
         }
 
+        [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
             if (!ModelState.IsValid)
