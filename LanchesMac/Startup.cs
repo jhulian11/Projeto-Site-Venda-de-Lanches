3bcfed7abf6a2075e4232b1af6daa1f363596658
jhulian11 <jhuliangamer@gmail.com>
2023-08-17 21:58:51 -0300

    Creating Relatory for Pedidos by Date Period

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -1,4 +1,5 @@
-﻿using LanchesMac.Context;
+﻿using LanchesMac.Areas.Admin.Servicos;
+using LanchesMac.Context;
 using LanchesMac.Models;
 using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
@@ -37,6 +38,7 @@ namespace LanchesMac
             services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+            services.AddScoped<RelatorioVendasService>();
 
             services.AddAuthorization(options =>
             {
